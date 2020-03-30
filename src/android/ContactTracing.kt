71c4bffaaddb2976eedf4e3f76bad780474f5862
Thomas Zachariah <tzachari@umich.edu>
2020-03-30 09:56:54 -0400

    Basic components for plugin based on covidwatch-android:libcontacttracing

---
+++ src/android/ContactTracing.kt
@@ -0,0 +1,15 @@
+package org.covidwatch.libcontacttracing
+
+import java.util.*
+import org.apache.cordova.*
+import org.json.*
+
+
+class ContactTracing : CordovaPlugin() {
+
+    @Throws(JSONException::class)
+    override fun execute(action: String, args: JSONArray, context: CallbackContext): Boolean {
+
+    }
+
+}
