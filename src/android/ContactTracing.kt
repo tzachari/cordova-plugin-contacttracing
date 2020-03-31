24b0ade086a914a83b1a1b3d395cf2507538a1e1
Thomas Zachariah <tzachari@umich.edu>
2020-03-30 23:06:05 -0400

    Add support for Kotlin

--- src/android/ContactTracing.kt
+++ src/android/ContactTracing.kt
@@ -10,6 +10,32 @@ class ContactTracing : CordovaPlugin() {
     @Throws(JSONException::class)
     override fun execute(action: String, args: JSONArray, context: CallbackContext): Boolean {
 
+        try {
+            when ( action ) {
+                "startScanner" -> {
+
+                }
+                "stopScanner" -> {
+
+                }
+                "startAdvertiser" -> {
+
+                }
+                "stopAdvertiser" -> {
+
+                }
+                "updateCEN" -> {
+
+                }
+                else -> throw Exception( "Invalid Action" )
+                context.success()
+            }
+            return true
+        } catch (e: Exception) {
+            context.error( e.toString() )
+            return false
+        }
+
     }
 
 }
